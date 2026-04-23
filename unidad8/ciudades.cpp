d819ca91a4161799f0113ec44be7d971fe96c770
diegosuth <dsubiabreonate@gmail.com>
2026-04-23 12:07:29 +0200

    dfs implementation(kinda)

--- unidad8/ciudades.cpp
+++ unidad8/ciudades.cpp
@@ -1,39 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool bfs(int startNode, int obj, const vector<vector<int>>& adj, int V){
-    vector<bool> visited(V, false);
-    vector<int> parent(V,-1); //guarda el camino final optimo
-    queue<int> proc;
-    visited[startNode] = true;
-    proc.push(startNode);
-    while(!proc.empty()){
-        int current = proc.front();
-        proc.pop();
-        for(int neighbour: adj[current]){//reviso todos los nodos conectados a current
-            if(!visited[neighbour]){
-                visited[neighbour] = true;
-                proc.push(neighbour);//guardo el encontrado para explorar sus vecinos luego
-                parent[neighbour] = current;
+using pii = pair<int,int>;
+bool bfs(int startNode, int edges, const vector<vector<int>>& adj, int V){//para revisar si todos estan conectados
+    vector<bool> nodevisited(V, false);
+    queue<int> nodeproc;//queue para procesar nodos
+    nodevisited[startNode] = true;
+    nodeproc.push(startNode);
+    while(!nodeproc.empty()){
+        int currentnode = nodeproc.front();
+        nodeproc.pop();
+        for(int neighbour: adj[currentnode]){//reviso todos los nodos conectados a current
+            if(!nodevisited[neighbour]){
+                nodevisited[neighbour] = true;
+                nodeproc.push(neighbour);//guardo el encontrado para explorar sus vecinos luego
             }
         }
     }
-    for(bool b : visited){//descarto si alguno esta desconectado
+    for(bool b : nodevisited){//descarto si alguno esta desconectado
         if(!b){
             return false;
         }
     }
-    vector<int>caminobest;
-    int lastone = obj;
-    while(lastone != -1){//si llego a -1 es el inicio, voy de final -> inicio
-        caminobest.push_back(lastone);
-        lastone = parent[lastone];//voy y reviso quien es el padre del ultimo, y luego 
-        //el padre de ese, y asi hasta llegar al inicio
-    }
-    for(int d : caminobest){
-        cout << d  + 1<< endl;
-    }
     return true;
 }
+bool isValid(int r, int c,int vertex,int edges) {
+    return r >= 0 && r < vertex  && c >= 0 && c < edges;
+}
+
+// DFS que retorna true si encontramos el camino
+bool DFS(int r, int c, int verticestots, int edges, vector<pii>& calles) {
+        
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -42,6 +40,7 @@ int main() {
     int start;
     cin >> verticestots >> edges >> start;
     vector<vector<int>> conexiones(verticestots);
+    vector<pii> calles(edges);
     start--;
     int a;
     int b;
@@ -49,6 +48,7 @@ int main() {
         cin >> a >> b;
         a--;
         b--;
+        calles[i] = {a,b};
         conexiones[a].push_back(b);
         conexiones[b].push_back(a);
     }
@@ -59,11 +59,11 @@ int main() {
             break;
         }
     }
-    if(!evendegree || !bfs(0,verticestots-1,conexiones,verticestots)){
+    if(!evendegree || !bfs(0,edges,conexiones,verticestots)){
         cout << -1 << endl;
     }
-    else{//agregar logica para revisar si estan todos conectados
-
+    else{
+        
     }
     return 0;    
 }
