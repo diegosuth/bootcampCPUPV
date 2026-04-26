34d7696a50517386bbf261e78a429612c2fe9ab5
diegosuth <dsubiabreonate@gmail.com>
2026-04-26 20:37:52 +0200

    addition of gitignore

---
+++ unidad9/copito.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int casos;
+    cin >> casos;
+    for(int i = 0; i < casos; i++) {
+        
+    }
+    
+    return 0;    
+}
