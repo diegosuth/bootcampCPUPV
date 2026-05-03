a735707ad390ec0204a7b8c59df545e731db26e5
diegosuth <dsubiabreonate@gmail.com>
2026-05-03 20:33:36 +0200

    variation of the first problem

--- unidad9/copito.cpp
+++ unidad9/copito.cpp
@@ -22,13 +22,22 @@ int main() {
         }
         int y; //the middle node the leaf is connected to
         int x;//the size of the adj list of the parent of y, x.(aka how many connections the root has)
+        int max = 0;
         for(int d = 0; d< nodos; d++){//first find a leaf to know its parent, therefore y(middle node)
             if(adj[d].size() == 1){
-                y = adj[adj[d][0]].size() - 1;//using the leaf as index, the size of its parent it's the connections 
-                break;//of the middle node, so its fundamental to substract 1 since the root doesnt count for y
+                y = adj[adj[d][0]].size() - 1;//using the leaf as index, the column of its parent it's the connections
+                //of the middle node, so its fundamental to substract 1 since the root doesnt count for y
+                //for(int f = 0; f < adj[adj[d][0]].size();f++){//alt code for finding root between middle connection
+                    //if(adj[adj[adj[d][0]][f]].size() > max){
+                        //max = adj[adj[adj[d][0]][f]].size();
+                    //}
+                //}
+                break;
             }
         }
         x = edges / (y + 1);
+        //alternatively:
+        //x = max;
         //totaledges = x + (x * y) since there are 3 levels, the total edges are the 
         //amount connected to the root x + these x nodes times their y amount of leafs
         //aka the size of the parent of y * x
